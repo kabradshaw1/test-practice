4237f0037cad71c2a46d7b76d4d0c663a1ddbe89
kabradshaw1 <itllbok82@gmail.com>
2023-02-23 06:26:38 -0500

    first test for About component

---
+++ src/components/About/__test__/index.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
+
+describe('About component', () => {
+  it('renders', () => {
+    render(<About />);
+  })
+})
+
+afterEach(cleanup);
