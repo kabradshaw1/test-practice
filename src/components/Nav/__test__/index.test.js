0e8775f9c935e0f6921a50c4a66d89853410cc4d
kabradshaw1 <itllbok82@gmail.com>
2023-02-23 08:18:13 -0500

    more testing for Nav

--- src/components/Nav/__test__/index.test.js
+++ src/components/Nav/__test__/index.test.js
@@ -13,6 +13,18 @@ describe('Nav component', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav/>);
     expect(asFragment()).toMatchSnapshot();
-  })
+  });
+});
 
+describe('emoji is visiable', () => {
+  it('inserts emoji into the h2', () => {
+    const { getByLabelTest } = render(<Nav/>);
+    expect(getByLabelTest('camera')).toHaveTextContent('📸');
+  });
+});
+
+describe('links are visable', () => {
+  it('inserts text into the links', () => {
+    
+  })
 })
